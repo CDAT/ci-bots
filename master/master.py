df757ece72a6764628fb321b9896b96970fc92e1
Charles Doutriaux <doutriaux1@llnl.gov>
2017-04-19 11:24:04 -0700

    updated list of commit

--- master/master.py
+++ master/master.py
@@ -11,6 +11,9 @@ import tangelo
 import requests
 
 import time
+import subprocess
+import shlex
+
 
 # load a projects file
 # see https://developer.github.com/webhooks/#events
@@ -23,11 +26,15 @@ with open(_projects_file) as f:
 def authenticate(key, body, received):
     """Authenticate an event from github."""
     computed = hmac.new(str(key), body, hashlib.sha1).hexdigest()
+    print "RECEIVED:",received
+    print "computed:",computed
     # The folowing func does not exist on my home mac
     # trapping in try/except
     try:
+      print "COMPARE:",hmac.compare_digest(computed, received)
       return hmac.compare_digest(computed, received)
-    except:
+    except Exception,err:
+      print "EXCETPED:",err
       return computed == received
 
 
@@ -75,7 +82,7 @@ def get(*arg, **kwarg):
 def post(*arg, **kwarg):
     """Listen for github webhooks, authenticate, and forward to buildbot."""
     # retrieve the headers from the request
-    print "MASTER RECEIVED A POST EVENT",args,kargs
+    print "MASTER RECEIVED A POST EVENT",arg,kwarg
     # print "TGELO CONFI",tangelo.cherrypy.request.header_list
     try:
         received = tangelo.request_header('X-Hub-Signature')[5:]
@@ -102,9 +109,7 @@ def post(*arg, **kwarg):
         return 'Unknown project'
 
     # make sure this is a valid request coming from github
-    if not authenticate(project.get('api-key', ''), body, received) \
-        and \
-        not authenticate(project.get('bot-key', ''), body, received):
+    if not authenticate(project.get('api-key', ''), body, received):
         tangelo.http_status(403, "Invalid signature")
         return 'Invalid signature'
 
@@ -113,126 +118,36 @@ def post(*arg, **kwarg):
 
     if project['github-events'] == '*' or event in project['github-events']:
         obj['event'] = event
-        f=open("events.json")
-        print >>f, event
-        f.close()
         try:
           commit = obj["head_commit"]
           is_commit = True
         except:
           commit = obj["pull_request"]
           is_commit = False
-          try:
-            act = obj["action"]
-            if act == "closed":  # closed PR no need to run anything
-              return "Closed PR, skipping"
-          except:
-            pass
-        if commit is None:
-          ## no head_Commit trying to see if it's a pull request
-          return "Null Head Commit Found, not a PR either skipping"
-        if is_commit:
-          commit_id = commit["id"]
-          commit_msg = commit["message"]
-        else:
-          ## It 's a PR faking the head_commit/id bits for slaves
-          commits_url = commit["commits_url"]
-          commit_id = commit["head"]["sha"]
-          commit_statuses_url=commit["statuses_url"]
-          commit_ref = commit["head"]["ref"]
-          resp = requests.get(commits_url,verify=False)
-          commit = resp.json()[-1]["commit"]
-          commit_msg=commit["message"]
-          commit["id"]=commit_id
-          obj["ref"]=commit_ref
-          commit["statuses_url"]=commit_statuses_url
-          obj["head_commit"]=commit
-        signature = hmac.new(str(project["bot-key"]), json.dumps(obj), hashlib.sha1).hexdigest()
+        print "commit was:",commit
+        update_wiki_commit(project["wiki_path"],commit)
+    else:
+        tangelo.http_status(200, "Unhandled event")
+        return 'Unhandled event'
 
-        if commit_msg.find("##bot##skip-commit")>-1:
-            # User requested to not send this commit to bots
-            return "Skipped testing commit '%s' at committer request (found string '##bot##skip-commit')"
-        nok = 0
-        for islave, slave in enumerate(project["slaves"]):
-          islaves = commit_msg.find("##bot##skip-slaves")
-          if islaves>-1:
-            # ok some slaves need skipping
-            msg = commit_msg[islaves+18:]
-            iend = msg.find("\n")
-            msg = msg[:iend].strip().split()
-            iskip = False
-            for m in msg:
-              if slave.find(m)>-1:
-                iskip = True
-                break
-            if iskip:
-              print "\033[%im" % (91+islave),"Commit asked to skip:",slave,"\033[0m"
-              nok+=1
-              continue
-          print "\033[%im" % (91+islave),"SENDING TO:",slave,"\033[0m"
-          try:
-            resp = forward(slave,obj,signature)
-            if resp.ok:
-              nok+=1
-          except:
-            print "\033[%im" % (91+islave),"could not connect","\033[0m"
-            nok+=1
 
-        if nok>0:
-          return "Ok sent this to %i slaves out of %i" % (nok,len(project["slaves"]))
-        else:
-          msg = "All slaves failed to respond, last error was: %s" % resp.text 
-          print msg
-          tangelo.http_status(resp.status_code, msg)
-          return msg
-    elif tangelo.request_header('BOT-Event') == "status":
-      ## put here code to update status of commit on github
-      headers = {
-          "Authorization":"token "+project["token"],
-          }
-      commit_id = obj["commit"]["id"]
-      if obj["code"] == 0:
-        state = "success"
-      elif obj["code"] is None:
-        state = "pending"
-      else:
-        state = "failure"
+def update_wiki_commit(path,commit):
+    fnm = os.path.join(path,"COMMITS.md")
+    with open(fnm) as f:
+        commits = f.readlines()[:50]
+        commits.insert(1,"%s %s %s" % (commit["id"],commit["author"]["username"],commit["message"].split("\n")[0][:25]))
+    f=open(fnm,"w")
+    f.write("\n".join(commits))
+    f.close()
+    subprocess.call(shlex.split("git commit -am 'updated list of commit'"))
+    subprocess.call(shlex.split("git push"))
 
-      slave = obj["slave_host"]
-      try:
-        islave = project["slaves"].find("http://"+slave)
-      except:
-        islave = -91  # Turn off styling
-      pth = os.path.join(project["logs_dir"],slave,project_name,commit_id)
-      print "\033[%im" % (91+islave),"DUMPING INFO IN:",pth,"\033[0m"
-      print "\033[%im" % (91+islave),"could not connect","\033[0m"
-      if not os.path.exists(str(pth)):
-        print "Creating:",pth
-        os.makedirs(pth)
-      f=open(os.path.join(pth,cmd2str(obj["command"])),"w")
-      print >>f,"<html><body>"
-      print >>f,"<h1>%s (%s)</h1><br><h2>commit: %s<h2>" % (project_name,obj["slave_name"],commit_id)
-      host = tangelo.cherrypy.url()
-      host=host[host.find("//")+2:]
-      if obj["previous"] is not None:
-        ptarget = "http://%s/%s/%s/%s/%s" % (host,slave,project_name,commit_id,cmd2str(obj["previous"]))
-        print >>f, "<h2>PREVIOUS COMMAND</h2>"
-        print >>f,"<a href='%s'>" % ptarget,obj["previous"],"</a>"
-      print >>f, "<h2>COMMAND</h2>"
-      print >>f,"<pre>",obj["command"],"</pre>"
-      if obj["command"].find("ctest")>-1:
-        print >>f, "<h3>CTEST PAGE</h3>"
-        build_name = "%s-%s" % (slave.replace(":",""),commit_id)
-        ptarget = "https://open.cdash.org/index.php?compare1=65&filtercount=2&field1=buildname%%2Fstring&project=UV-CDAT&field2=buildstarttime%%2Fdate&value1=%s" % build_name
-        print >>f,"<A HREF='%s'>Click here</A>" % ptarget
-      print >>f, "<h3>OUTPUT</h3>"
-      print >>f,"<pre>",obj["output"],"</pre>"
-      print >>f, "<h3>ERROR</h3>"
-      print >>f,"<pre>",obj["error"],"</pre>"
-      print >>f,"</body></html>"
-      f.close()
-      target = "http://%s/%s/%s/%s/%s" % (host,slave,project_name,commit_id,cmd2str(obj["command"]))
 
+if __name__ == "__main__":
+    update_wiki_commit("/Users/doutriaux1/git/mpas-dummy.wiki",{"id":"121234324234","author":{"username":"doutriaux1"},"message":"my long commit is here"})
+
+
+def crap():
       context = "cont-int/LLNL/%s-%s" % (obj["os"],obj["slave_name"])
       data = {
           "state":state,
@@ -247,9 +162,6 @@ def post(*arg, **kwarg):
           headers = headers)
 
       return "Received and treated a BOT STATUS update event"
-    else:
-        tangelo.http_status(200, "Unhandled event")
-        return 'Unhandled event'
 
 
 def cmd2str(command):
