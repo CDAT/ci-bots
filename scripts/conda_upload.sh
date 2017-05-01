4eb816b0d264378c2de5b0676e3b0a215907f575
Charles Doutriaux <doutriaux1@llnl.gov>
2017-05-01 13:21:44 -0700

    correct pkg name and os (noarch)

--- scripts/conda_upload.sh
+++ scripts/conda_upload.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
-PKG_NAME=cdms2
+PKG_NAME=cibots
 USER=uvcdat
-if [ `uname` == "Linux" ]; then
-    OS=linux-64
-    echo "Linux OS"
-else
-    OS=osx-64
-    echo "Mac OS"
-fi
 
 export PATH="$HOME/miniconda/bin:$PATH"
 mkdir ~/conda-bld
@@ -19,4 +12,4 @@ git clone git://github.com/UV-CDAT/conda-recipes
 cd conda-recipes
 python ./prep_for_build.py
 conda build cibots -c conda-forge
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d.%H.%M`.*-py_0.tar.bz2 --force
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/noarch/$PKG_NAME-`date +%Y.%m.%d.%H.%M`.*-py_0.tar.bz2 --force
