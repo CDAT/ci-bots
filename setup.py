c989f1486bd542e80f349e250b355b4d2a6e38ca
Charles Doutriaux <doutriaux1@llnl.gov>
2017-06-21 18:30:51 -0700

    for zenodo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup (name = "cibot",
        author="Charles Doutriaux",
-       version="0.2",
+       version="0.3",
        description = "Utilities for git/github continuous integration",
        url = "http://github.com/uv-cdat/ci-bot",
        packages = ['cibot'],
